e068e305fcd21dc0ede1721d06f112db02eccd4c
franciscokeppesantos <franciscokeppesantos@gmail.com>
2025-01-14 16:48:45 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Aplicativo());
+}
+
+class Aplicativo extends StatelessWidget {
+  const Aplicativo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Layout',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        useMaterial3: true,
+      ),
+      home: const PaginaPrincipal(title: 'Aplicação Layout'),
+    );
+  }
+}
+
+class PaginaPrincipal extends StatefulWidget {
+  const PaginaPrincipal({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<PaginaPrincipal> createState() => _PaginaPrincipalState();
+}
+
+class _PaginaPrincipalState extends State<PaginaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                // color: Colors.amber,
+                child: const Text('Talento Tech'),
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                        // alignment: Alignment.center,
+                        // color: Colors.red,
+                        // child: const Text('Primeira Coluna'),
+                        ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.circular(25),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1,
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            blurRadius: 25,
+                            offset: Offset(5, 5),
+                          ),
+                        ],
+                      ),
+                      child: const Text('Aplicativo'),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                        // alignment: Alignment.center,
+                        // color: Colors.red,
+                        // child: const Text('Terceira Coluna'),
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                alignment: Alignment.center,
+                //color: Colors.blue,
+                child: const Text('Bocaiúva do Sul'),
+              ),
+            ),
+          ],
+        ),
+      ),
+     // floatingActionButton: FloatingActionButton(
+     //   onPressed: () {},
+     //   tooltip: 'Increment',
+    //    child: const Icon(Icons.add),
+     // ),
+    );
+  }
+}
